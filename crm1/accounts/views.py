387718c8b4a0242e727c05f338cb9333f06253b2
nhatdang2604 <nhatdang2604@gmail.com>
2022-04-22 22:50:55 +0700

    Finish pt 16 + 17 + 18 + 19

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import *
 from .filters import OrderFilter
 from .decorators import *
 
@@ -25,9 +25,13 @@ def register(request):
             user = form.save()
             username = form.cleaned_data.get('username')
             
-            group = Group.objects.get(name = "customer")
-            user.groups.add(group)
-            
+            # group = Group.objects.get(name = "customer")
+            # user.groups.add(group)
+            # Customer.objects.create(
+            #     user = user,
+            #     name = username,
+            # )
+
             messages.success(request, "Account was created for " + username)
 
             return redirect('login')
@@ -60,9 +64,44 @@ def logoutUser(request):
     logout(request)
     return redirect("login")
 
+@login_required(login_url="login")
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    deliver = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+    
+    context = {
+        'orders': orders,
+        'total_orders': total_orders,
+        'deliver': deliver, 
+        'pending': pending,
+        }
+
     return render(request, 'accounts/user.html', context)
+
+@login_required(login_url="login")
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+
+    customer = request.user.customer
+    form = CustomerForm(instance = customer)
+    
+    if "POST" == request.method:
+        form = CustomerForm(
+            request.POST,
+            request.FILES,
+            instance = customer)
+        
+        if form.is_valid():
+            form.save()
+    
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/account_settings.html", context)
 
 @login_required(login_url="login")
 @admin_only
--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #my app
-    'accounts',
+    'accounts.apps.AccountsConfig',
 
     #support stuffs
     'django_filters',
@@ -129,6 +129,8 @@ MEDIA_URL = 'images/'
 STATICFILES_DIRS = [
    BASE_DIR/'static',
 ]
+
+MEDIA_ROOT = BASE_DIR/'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
